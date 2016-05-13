31ecee5fe875bf683453916ac4880b94d234c232
Gints Polis <gints.polis@lattelecom.lv>
2016-05-13 16:36:54 +0300

    Allow cross domain requests

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,12 @@ def create_app(config_filename):
     from app.users.views import users
     app.register_blueprint(users, url_prefix='/api/v1/users')
     
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+        return response
 
     return app
     
